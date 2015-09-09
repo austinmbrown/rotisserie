4b2b9e2cdfa5e245f2c46136ab284752b2ad8004
Austin Brown <austinmbrown@gmail.com>
2015-09-09 09:56:59 -0400

    automatically figure out what week it is

--- client/app/account/login/login.controller.js
+++ client/app/account/login/login.controller.js
@@ -15,7 +15,7 @@ angular.module('rotisserieApp')
         })
         .then( function() {
           // Logged in, redirect to home
-          $location.path('/week/' + thisweek);
+          $location.path('/weeks');
         })
         .catch( function(err) {
           $scope.errors.other = err.message;
@@ -28,32 +28,5 @@ angular.module('rotisserieApp')
     };
   });
 
-// The following code figures out what week is is within the 2015 season.
-var now = moment();
-// This array represents the ending dates for each week.
-var weeks = [
-  "2015-09-15",
-  "2015-09-22",
-  "2015-09-29",
-  "2015-10-06",
-  "2015-10-13",
-  "2015-10-20",
-  "2015-10-27",
-  "2015-11-03",
-  "2015-11-10",
-  "2015-11-17",
-  "2015-11-24",
-  "2015-12-01",
-  "2015-12-08",
-  "2015-12-15",
-  "2015-12-22",
-  "2015-12-29",
-  "2016-01-04"
-];
-for (var i = 1; i < weeks.length; i++) {
-  if (now.isBefore(moment(weeks[i]))) {
-    var thisweek = i;
-    break;
-  }
-}
+
 
--- client/app/week/week.controller.js
+++ client/app/week/week.controller.js
@@ -2,7 +2,7 @@
 
 angular.module('rotisserieApp')
   .controller('WeekCtrl', function ($scope, $http, Auth, User) {
-    $scope.weekSelection = 1;
+    $scope.weekSelection = thisweek;
     $scope.weeks = {};
     var user = Auth.getCurrentUser();
     console.log(user._id);
@@ -23,7 +23,36 @@ angular.module('rotisserieApp')
         $scope.weeks[$scope.weekSelection - 1] = weekData;
       })
     })
-  })
+  });
 
+
+// The following code figures out what week is is within the 2015 season.
+var now = moment();
+// This array represents the ending dates for each week.
+var weeks = [
+  "2015-09-15",
+  "2015-09-22",
+  "2015-09-29",
+  "2015-10-06",
+  "2015-10-13",
+  "2015-10-20",
+  "2015-10-27",
+  "2015-11-03",
+  "2015-11-10",
+  "2015-11-17",
+  "2015-11-24",
+  "2015-12-01",
+  "2015-12-08",
+  "2015-12-15",
+  "2015-12-22",
+  "2015-12-29",
+  "2016-01-04"
+];
+for (var i = 1; i < weeks.length; i++) {
+  if (now.isBefore(moment(weeks[i]))) {
+    var thisweek = i;
+    break;
+  }
+}
 
 
